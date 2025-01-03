56f6a6de94361ee2510ab452d66eb8208dc7205f
michael stepniak <michaelstepniak@michaels-mbp.mynetworksettings.com>
2025-01-03 13:13:11 -0500

    add generic view

--- django-rest-mini-project/project/api/mysite/api/views.py
+++ django-rest-mini-project/project/api/mysite/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer 
 
 # Create your views here.
+class BlogPostListCreate(generics.ListCreateAPIView):
+  # Get all BlogPost objects that exist using the ORM.
+  queryset = BlogPost.objects.all()
+
+  # Specify which serializer we want to use.
+  serializer_class = BlogPostSerializer 
