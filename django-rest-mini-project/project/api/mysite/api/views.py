a557c76ba5ff606e8c172f919a67a78d61129922
michael stepniak <michaelstepniak@michaels-mbp.mynetworksettings.com>
2025-01-06 15:57:13 -0500

    create update-destroy view

--- django-rest-mini-project/project/api/mysite/api/views.py
+++ django-rest-mini-project/project/api/mysite/api/views.py
@@ -5,8 +5,18 @@ from .serializers import BlogPostSerializer
 
 # Create your views here.
 class BlogPostListCreate(generics.ListCreateAPIView):
-  # Get all BlogPost objects that exist using the ORM.
-  queryset = BlogPost.objects.all()
+    # Get all BlogPost objects that exist using the ORM.
+    queryset = BlogPost.objects.all()
 
-  # Specify which serializer we want to use.
-  serializer_class = BlogPostSerializer 
+    # Specify which serializer we want to use.
+    serializer_class = BlogPostSerializer 
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    # Copy the queryset and serializer.
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+    # pk is "primary key," which is the id of the blogpost.
+    lookup_field = "pk"
+
+
